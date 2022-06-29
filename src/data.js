79896357e31e2cc39b53a36582879ced88a7745a
Shaswat <shaswatkr96@gmail.com>
2022-06-29 23:42:57 +0530

    Creating mock data

---
+++ src/data.js
@@ -0,0 +1,11 @@
+import imgDeepa from "./assets/profilePics/Deepa.jpeg";
+import imgShaswat from "./assets/profilePics/Shaswat.jpg";
+import imgYoga from "./assets/profilePics/yoga.jpg";
+
+const data = [
+    { id: 1, img: imgDeepa, title: "IT lesson with Deepa Keshri", peopleRated: 30, rating: 5, cost: 100, soldOut: 0 },
+    { id: 2, img: imgShaswat, title: "Live Life with Shaswat Kumar", peopleRated: 6, rating: 4.5, cost: 10, soldOut: 22 },
+    { id: 3, img: imgYoga, title: "Yoga Lessons", peopleRated: 100, rating: 4.0, cost: 20, soldOut: 7 }
+];
+
+export default data;
