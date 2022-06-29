9100607f56f1f849f7cf34ca32bcfe4901a225ab
Shaswat <shaswatkr96@gmail.com>
2022-06-29 23:44:19 +0530

    Creating NavBar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import logo from "../assets/logo.png";
+
+const Navbar = () => {
+    return (
+        <nav>
+            <img src={logo} alt="Air Bnb logo" />
+        </nav>
+    );
+};
+
+export default Navbar;
