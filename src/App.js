92611306194a30710ed936a8d29226477d052d62
Shaswat <shaswatkr96@gmail.com>
2022-06-29 23:45:18 +0530

    Adding all the components into App and style it with index.css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
+
+import { useState } from 'react';
+
 import './App.css';
+import data from "./data";
+
+import Card from './components/Card';
+import Hero from './components/Hero';
+import Navbar from './components/Navbar';
 
 function App() {
+    const [cardData, setCardData] = useState(data);
+
+    console.log(cardData);
+
+    const CardModel = cardData.map(eachCard => <Card key={eachCard.id} data={eachCard} />);
+
     return (
-        <div className="App">
-            <h1> Hello World </h1>
-        </div>
+        <>
+            <Navbar />
+            <Hero />
+
+            <div className="cards">
+                {CardModel}
+            </div>
+        </>
     );
 }
 
